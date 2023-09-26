5f7538b3459f5dcfbd170440f27e03c28c6f3167
Caesar Chi <clonncd@gmail.com>
2023-09-26 13:07:51 +0800

    init vue3 example

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue 3 Pinia
+
+## Usage
+
+```sh
+yarn
+yarn dev
+```
+
+[Open the local server](http://localhost:3000) and inspect using the [Vue Devtools beta](https://chrome.google.com/webstore/detail/vuejs-devtools/ljjemllljcmogpfapbkkighbhhppjdbg?hl=en).
+
---
+++ src/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>Exaple Pinia</h1>
+    <p>The store for Vue</p>
+  </header>
+
+  <slot />
+
+  <hr />
+
+  <footer>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { version as piniaVersion } from 'pinia/package.json'
+import { version as vueVersion } from 'vue/package.json'
+</script>
+
+<style scoped>
+.logo {
+  width: 1.5rem;
+  height: 1.5rem;
+  max-width: 100%;
+  margin-bottom: 0;
+  color: white;
+}
+</style>
